e7290e2e9bf9b712e06c1a581762447b49b3b531
srilathadinakar <srilathadinakar@gmail.com>
2024-03-31 13:05:46 +0530

    updated

--- script.js
+++ script.js
@@ -9,17 +9,14 @@ request.onload=function(){
 var region = result.filter((ele)=>ele.region==="Asia")
 var countrynames = region.map((ele)=>console.log(ele.name.common));
 
-
 //b.Get all the countries with a population of less than 2 lakhs using Filter function
 var pop = result.filter((ele)=>ele.population<200000)
 var popnames = pop.map((ele)=>console.log(ele.name.common,ele.population));
-
 
 //c.Print the following details name, capital, flag, using forEach function
 var details = result.forEach((ele)=>console.log("Name:"+" "+ele.name.common))
 var capital = result.forEach((ele)=>console.log("Capital:"+ele.capital))
 var flag = result.forEach((ele)=>console.log("Flags:"+ele.flags.png))
-
 
 //d.Print the total population of countries using reduce function
 var pop1 = result.map((ele)=>ele.population);
@@ -27,7 +24,6 @@ const totalpop = pop1.reduce((sum,i)=>{
     return sum+i;
  },0)
  console.log("Total Population:"+totalpop);
-
 
 //e.Print the country that uses US dollars as currency.
 var cur = result.filter((ele)=>ele.currencies);
